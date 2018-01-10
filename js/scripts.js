4bf110a81e5743062093d360cb33cb150a6205ed
Drew Tucker and Eric Swotinsky <eric@>
2018-01-10 14:02:04 -0800

    Add modal to eventually confirm appointment details.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
+
     var blanks = ["userName", "userProblem", "userDate", "userStartTime", "userEndTime"];
 
     blanks.forEach(function(blank) {
       var userInput = $("input#" + blank).val();
       $("." + blank).text(userInput).val();
     })
+
+    event.preventDefault();
   })
 
 })
